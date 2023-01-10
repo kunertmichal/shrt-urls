e1a1e6f606db0a31db4190299d1a3314730b0690
Michal Kunert <30593515+kunertmichal@users.noreply.github.com>
2023-01-10 12:54:35 +0100

    add tests of urlsStorage

--- src/urls/urls.storage.spec.ts
+++ src/urls/urls.storage.spec.ts
@@ -1,1 +1,27 @@
-describe('UrlsStorage', () => {});
+import { Test } from '@nestjs/testing';
+import { UrlsStorage } from './urls.storage';
+import { ConfigService } from '@nestjs/config';
+
+describe('UrlsStorage', () => {
+  let urlsStorage: UrlsStorage;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [UrlsStorage, ConfigService],
+    }).compile();
+
+    urlsStorage = module.get<UrlsStorage>(UrlsStorage);
+  });
+
+  describe('onApplicationBootstrap', () => {
+    it('should be defined', () => {
+      expect(urlsStorage.onApplicationBootstrap).toBeDefined();
+    });
+  });
+
+  describe('onApplicationShutdown', () => {
+    it('should be defined', () => {
+      expect(urlsStorage.onApplicationShutdown).toBeDefined();
+    });
+  });
+});
